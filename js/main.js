a3051f72dd087722df8826ade348a0d2bd43545d
olsi10 <wangbampang2035@gmail.com>
2023-03-11 23:35:31 +0900

    찜 O 하트 <-> 찜 X 하트 클릭 이벤트 [테스트]

--- js/main.js
+++ js/main.js
@@ -1,4 +1,17 @@
 // 찜 클릭 시 하트 채워지는 기능 (빈 하트 -> 찬 하트 이미지 변경)
+
+// 하트 변수
+let heart = document.querySelector(".heart");
+
 function toggleImg() {
-    document.getElementsByClassName("heart").src = ("./img/books-heart-2.png");
+    // console.log(heart.getAttribute("sr/c"));
+
+    // 꽉 찬 하트일 때
+    if(heart.getAttribute("src") == "./img/books-heart-1.png") { // 경로 가져오기
+        heart.setAttribute("src", "./img/books-heart-2.png");
+    }
+    // 빈 하트일 때
+    else {
+        heart.setAttribute("src", "./img/books-heart-1.png");
+    }
 }
