819b1fc42b612d5d8fd3a248bec507949fda1d1b
olsi10 <wangbampang2035@gmail.com>
2023-04-15 23:54:05 +0900

    foreach문 -> 여러 개 요소에 같은 이벤트 등록

--- js/slider.js
+++ js/slider.js
@@ -1,27 +1,26 @@
-function slider() {
-  let nextBtn = document.getElementById("next-btn");
-  // let prevBtn = document.getElementsByClassName("prev-btn")[0];
-  let container = document.getElementsByClassName("books-container")[0];
-  let i = 1;
+let nextBtn = document.querySelectorAll(".next-btn");
+// let prevBtn = document.getElementsByClassName("prev-btn")[0];
+let container = document.querySelectorAll(".books-container")[0];
+let i = 1;
 
-  // 다음 슬라이드
-  nextBtn.addEventListener("click", function () {
-    console.log("클릭");
-    let current = i * -1200;
-    container.style.transform = `translateX(${current}px)`;
-    container.style.transition = "all ease 1s";
-    i++;
-    console.log(`현재 위치 ${current}, ${i}`);
+nextBtn.forEach((nextBtns) => {
+  nextBtns.addEventListener("click", nextSlide);
+});
 
-    // 만약 마지막 슬라이드에 도달했다면 첫 슬라이드로 0.5초 빠르게 이동
-    if (current == -3600) {
-      container.style.transform = `translateX(0px)`;
-      container.style.transition = "all ease 0.5s";
-      prevBtn.style.display = "none";
-      i = 1;
-      console.log("첫 슬라이드 이동");
-    }
-  });
+function nextSlide() {
+  console.log("클릭");
+  let current = i * -1200;
+  container.style.transform = `translateX(${current}px)`;
+  container.style.transition = "all ease 1s";
+  i++;
+  console.log(`현재 위치 ${current}, ${i}`);
+
+  // 만약 마지막 슬라이드에 도달했다면 첫 슬라이드로 0.5초 빠르게 이동
+  if (current == -3600) {
+    container.style.transform = `translateX(0px)`;
+    container.style.transition = "all ease 0.1s";
+    // prevBtn.style.display = "none";
+    i = 1;
+    console.log("첫 슬라이드 이동");
+  }
 }
-
-slider();
